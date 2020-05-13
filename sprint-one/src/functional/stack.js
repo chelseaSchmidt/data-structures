9fc237b3b2cffa445f68f808896a12f4be3a16cd
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-13 12:51:21 -0700

    Finished implementation of functional pattern stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -8,15 +8,23 @@ var Stack = function() {
   someInstance.push = function(value) {
     //add a string to top of the stack
     //get max key + 1
+    let keys = Object.keys(storage);
+    let nextAvailable = Math.max(0,...keys) + 1;
     //add property at max key + 1 set to input value
+    storage[nextAvailable] = value;
   };
 
   someInstance.pop = function() {
     //remove and return string on the top of the stack
     //get max key
+    let keys = Object.keys(storage);
+    let maxKey = Math.max(...keys);
     //store value at max key
+    let value = storage[maxKey];
     //delete property at max key
+    delete storage[maxKey];
     //return stored value
+    return value;
   };
 
   someInstance.size = function() {
