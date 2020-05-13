75b4678efbe7eb5647beb3a5bc6036df12e39286
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-13 16:16:03 -0700

    Removed pseudocode from pseudoclassical Stack function

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,37 +3,27 @@ var Stack = function() {
 };
 
 Stack.prototype.push = function(string) {
-  //filter for numeric keys
   let numericKeys = Object.keys(this).filter(function(key) {
     return Number(key);
   });
-  //get max key
   let maxKey = Math.max(0, ...numericKeys);
-  //add string at max key + 1
   this[maxKey + 1] = string;
 };
 
 Stack.prototype.pop = function() {
-  //filter for numeric keys
   let numericKeys = Object.keys(this).filter(function(key) {
     return Number(key);
   });
-  //get max key
   let maxKey = Math.max(0, ...numericKeys);
-  //store value at max key
   let popped = this[maxKey];
-  //remove max key
   delete this[maxKey];
-  //return stored value
   return popped;
 };
 
 Stack.prototype.size = function() {
-  //filter for numeric keys
   let numericKeys = Object.keys(this).filter(function(key) {
     return Number(key);
   });
-  //return length of filtered keys array
   return numericKeys.length;
 };
 
