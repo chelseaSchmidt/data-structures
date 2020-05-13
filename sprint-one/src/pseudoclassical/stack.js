7beaf6e7af3c24a5a53c3d01674238873aa7879d
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-13 16:14:41 -0700

    Implemented Stack in pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,40 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.name = 'newStack';
+};
+
+Stack.prototype.push = function(string) {
+  //filter for numeric keys
+  let numericKeys = Object.keys(this).filter(function(key) {
+    return Number(key);
+  });
+  //get max key
+  let maxKey = Math.max(0, ...numericKeys);
+  //add string at max key + 1
+  this[maxKey + 1] = string;
+};
+
+Stack.prototype.pop = function() {
+  //filter for numeric keys
+  let numericKeys = Object.keys(this).filter(function(key) {
+    return Number(key);
+  });
+  //get max key
+  let maxKey = Math.max(0, ...numericKeys);
+  //store value at max key
+  let popped = this[maxKey];
+  //remove max key
+  delete this[maxKey];
+  //return stored value
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  //filter for numeric keys
+  let numericKeys = Object.keys(this).filter(function(key) {
+    return Number(key);
+  });
+  //return length of filtered keys array
+  return numericKeys.length;
 };
 
 
