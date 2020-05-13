047ba687774d492892d6cbcfdd1b7e1cc6f5c388
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-13 12:48:31 -0700

    Pseudocoded functional pattern stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,21 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //add a string to top of the stack
+    //get max key + 1
+    //add property at max key + 1 set to input value
   };
 
   someInstance.pop = function() {
+    //remove and return string on the top of the stack
+    //get max key
+    //store value at max key
+    //delete property at max key
+    //return stored value
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
