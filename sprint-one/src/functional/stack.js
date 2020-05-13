7fc1eb13c5001907f9060f09108b2d84cdbebb86
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-13 12:52:56 -0700

    Removed pseudocode from functional pattern stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,29 +1,19 @@
 var Stack = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
   someInstance.push = function(value) {
-    //add a string to top of the stack
-    //get max key + 1
     let keys = Object.keys(storage);
     let nextAvailable = Math.max(0,...keys) + 1;
-    //add property at max key + 1 set to input value
     storage[nextAvailable] = value;
   };
 
   someInstance.pop = function() {
-    //remove and return string on the top of the stack
-    //get max key
     let keys = Object.keys(storage);
     let maxKey = Math.max(...keys);
-    //store value at max key
     let value = storage[maxKey];
-    //delete property at max key
     delete storage[maxKey];
-    //return stored value
     return value;
   };
 
