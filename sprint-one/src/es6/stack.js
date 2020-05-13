efd16b05a9c3f410a89fbeaa6629091e598ccc1a
Chelsea Schmidt <cschmidt331@gmail.com>
2020-05-13 16:47:04 -0700

    Implemented Stack function in ES6 instantiation pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,42 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.name = 'newStack';
   }
 
 }
+
+Stack.prototype.push = function(string) {
+  //filter instance's keys for numeric keys
+  let numericKeys = Object.keys(this).filter(function(key) {
+    return Number(key);
+  });
+  //get max key
+  let maxKey = Math.max(0, ...numericKeys);
+  //add string at max key + 1
+  this[maxKey + 1] = string;
+};
+
+Stack.prototype.pop = function() {
+  //filter instance's keys for numeric keys
+  let numericKeys = Object.keys(this).filter(function(key) {
+    return Number(key);
+  });
+  //get max key
+  let maxKey = Math.max(0, ...numericKeys);
+  //store value at max key
+  let popped = this[maxKey];
+  //remove max key
+  delete this[maxKey];
+  //return stored value
+  return popped;
+};
+
+Stack.prototype.size = function() {
+  //filter instance's keys for numeric keys
+  let numericKeys = Object.keys(this).filter(function(key) {
+    return Number(key);
+  });
+  //return length of numeric keys collection
+  return numericKeys.length;
+};
